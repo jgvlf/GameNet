ae53d7dafb81ee9ed73dc7b8d21ec7fceb903cca
joaogabrielvlf <joaogabrielvlf@gmail.com>
2022-11-21 15:02:02 -0300

    👌 IMPROVE: Set To Run de API with the File

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ from fastapi import FastAPI
 app = FastAPI(title="GameNet API",
               description="A bet API for the World Cup Games")
 
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run('main:app', host='127.0.0.1', port=8000,
+                log_level='info', reload=True)
