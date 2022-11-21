76a9e9e2e79a9502198c3790025f68ba28706862
joaogabrielvlf <joaogabrielvlf@gmail.com>
2022-11-21 16:37:02 -0300

    👌 IMPROVE: Update Main File

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from core.configs import settings
+from api.v1.api import api_router
 
 app = FastAPI(title="GameNet API",
               description="A bet API for the World Cup Games")
+app.include_router(api_router, prefix=settings.API_V1_STR)
 
 if __name__ == '__main__':
     import uvicorn
